7ed78db9b866d82fb3b0a09522a07ed84b559837
Leon Pierre <Leon.Pierre@HealthTrustWS.com>
2020-04-20 11:44:55 -0400

    - fixed library exports

--- lib/dev_libraries.dart
+++ lib/dev_libraries.dart
@@ -1,3 +1,9 @@
 library dev_libraries;
 
-//export 'bloc/adbloc.dart';
+export 'bloc/adbloc.dart';
+export 'bloc/blocmanager.dart';
+export 'bloc/events.dart';
+export 'bloc/states.dart';
+
+export 'services/logging/appspectorservice.dart';
+export 'services/analytics/facebookappeventservice.dart';
