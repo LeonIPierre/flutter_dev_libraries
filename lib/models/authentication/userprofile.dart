f06766df95abc2e3a4ce05af2e9ea975e0530193
LeonPierre <lepierre08@gmail.com>
2021-02-28 13:11:40 -0500

    - added userprofile

---
+++ lib/models/authentication/userprofile.dart
@@ -0,0 +1,14 @@
+import 'package:dev_libraries/models/address.dart';
+import 'package:equatable/equatable.dart';
+
+class UserProfile extends Equatable {
+  final List<Address> addresses;
+
+  const UserProfile({this.addresses});
+
+  @override
+  List<Object> get props => [addresses];
+
+  factory UserProfile.fromJson(Map<String, dynamic> json) =>
+      UserProfile(addresses: json['addresses']);
+}
