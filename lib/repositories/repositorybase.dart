17339dc2c9ce2591a1e45ce227ff0d8758c99467
LeonPierre <lepierre08@gmail.com>
2022-02-24 13:26:49 -0500

    added toMap method

--- lib/repositories/repositorybase.dart
+++ lib/repositories/repositorybase.dart
@@ -2,18 +2,17 @@
 abstract class RepositoryBase<T, U> {
   String get tableName;
   
-  Future<Iterable<T>> getAll();
-
-  Future<T> get(String id);
-
   Future<U> add(T entity);
 
   Future<U> addAll(Iterable<T> entiites);
 
-  Future<U> update(T entity);
-
   Future<U> delete(T entity);
 
-  // ignore: unused_element
-  Map<String, Object?> _toMap(T item);
+  Future<T> get(String id);
+
+  Future<Iterable<T>> getAll();
+
+  Future<U> update(T entity);
+
+  Map<String, Object?> toMap(T entity);
 }
