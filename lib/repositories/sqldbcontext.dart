ef9e43e9a907dfa8e20a234de6a40186c220a202
Leon Pierre <lepierre08@gmail.com>
2022-09-03 11:51:06 -0400

    renamed varaibles for subclass access

--- lib/repositories/sqldbcontext.dart
+++ lib/repositories/sqldbcontext.dart
@@ -7,26 +7,26 @@ typedef EntityToMapCreator<T> = Map<String, Object?> Function(T entity);
 
 abstract class SqlDbContext<T extends PrimaryKeyIdentifier>
     extends RepositoryContext<T, int> {
-  final Database _database;
+  final Database database;
 
-  final EntityToMapCreator<T> _entityToMapCreator;
+  final EntityToMapCreator<T> entityToMapCreator;
 
-  final EntityFromMapCreator<T> _entityFromMapCreator;
+  final EntityFromMapCreator<T> entityFromMapCreator;
 
   SqlDbContext(
-      this._database, this._entityFromMapCreator, this._entityToMapCreator);
+      this.database, this.entityFromMapCreator, this.entityToMapCreator);
 
   @override
   Future<int> add(T entity) =>
-      _database.insert(tableName, _entityToMapCreator(entity));
+      database.insert(tableName, entityToMapCreator(entity));
 
   @override
   Future<int> addAll(Iterable<T> entities) =>
-      _database.transaction((transaction) async {
+      database.transaction((transaction) async {
         final Batch batch = transaction.batch();
 
         for (final entity in entities) {
-          batch.insert(tableName, _entityToMapCreator(entity));
+          batch.insert(tableName, entityToMapCreator(entity));
         }
 
         return batch.commit().then((insertedIds) => insertedIds.length);
@@ -34,25 +34,25 @@ abstract class SqlDbContext<T extends PrimaryKeyIdentifier>
 
   @override
   Future<int> delete(T entity) =>
-      _database.delete(tableName, where: 'id = ?', whereArgs: [entity.id]);
+      database.delete(tableName, where: 'id = ?', whereArgs: [entity.id]);
 
   @override
-  Future<T> get(String id) => _database.query(tableName,
+  Future<T> get(String id) => database.query(tableName,
       where: 'id = ?',
-      whereArgs: [id]).then((result) => _entityFromMapCreator(result.first));
+      whereArgs: [id]).then((result) => entityFromMapCreator(result.first));
 
   @override
   Future<Iterable<T>> getAll(Iterable<T> entities) {
     var entityIds = entities.map((e) => e.id);
-    return _database.query(tableName,
+    return database.query(tableName,
         where: 'id IN (${entityIds.join(',')})',
         whereArgs: [
           entityIds
-        ]).then((result) => result.map((e) => _entityFromMapCreator(e)));
+        ]).then((result) => result.map((e) => entityFromMapCreator(e)));
   }
 
   @override
   Future<int> update(T entity) =>
-      _database.update(tableName, _entityToMapCreator(entity),
+      database.update(tableName, entityToMapCreator(entity),
           where: 'id = ?', whereArgs: [entity.id]);
 }
